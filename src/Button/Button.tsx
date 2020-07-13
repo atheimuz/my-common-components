3d85091e6a301e2a0a334780b544169977bc1ef6
atheimuz <atheimuz@naver.com>
2020-07-13 20:41:09 +0900

    Button 컴포넌트 스타일 수정

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import cx from "classnames";
-import "./Button.scss";
+import React from "react"
+import cx from "classnames"
+import "./Button.scss"
 
 interface Props {
-  children: any,
-  className: string,
-  theme: string
+    children: any
+    className: string
+    theme: string
+    size: string
+    disabled: boolean
 }
 
-const Button = ({ children, className, theme }: Props) => {
-  return <button type="button" className={cx("my-button", className, theme)}>{children}</button>
+const Button = ({
+    children,
+    className,
+    theme = "default",
+    size = "middle",
+    disabled
+}: Props) => {
+    return (
+        <button
+            type="button"
+            className={cx("my-button", className, theme, size)}
+            disabled={disabled}
+        >
+            <div>{children}</div>
+        </button>
+    )
 }
 
-export default Button;
+export default Button
