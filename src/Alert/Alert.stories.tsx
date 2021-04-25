1974bcd191c1b89b90c0c8e3bf3b87fadfe26e2c
atheimuz <atheimuz@naver.com>
2021-04-25 16:27:03 +0900

    Alert 컴포넌트, 스토리북 추

---
+++ src/Alert/Alert.stories.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { Story, Meta } from "@storybook/react"
+
+import Alert, { Props } from "./Alert"
+
+export default {
+    title: "Example/Alert",
+    component: Alert
+} as Meta
+
+const Template: Story<Props> = (args) => <Alert {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+    children: "다시 시도해주세요."
+}
+Default.argTypes = {
+    status: {
+        control: {
+            type: "boolean"
+        }
+    }
+}
---
+++ src/Alert/Alert.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react"
+import cx from "classnames"
+import "./Alert.scss"
+
+interface Props {
+    children: any
+    className: string
+    status: boolean
+}
+
+const Alert = ({ className, children, status, ...rest }: Props) => {
+    const [isActive, setIsActive] = useState(false)
+
+    useEffect(() => {
+        if (!status) {
+            setTimeout(() => {
+                setIsActive(status)
+            }, 1000)
+        } else {
+            setIsActive(status)
+        }
+    }, [status])
+    return (
+        <div
+            className={cx("my-alert", className, { active: isActive })}
+            {...rest}
+        >
+            {children}
+        </div>
+    )
+}
+
+export default Alert
