9769089d264e474750620e7b22ad0dabc01adfb4
atheimuz <atheimuz@naver.com>
2021-05-01 20:50:25 +0900

    PlayListCard 컴포넌트 추가

---
+++ src/PlayListCard/PlayListCard.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import cx from "classnames";
+import "./PlayListCard.scss";
+
+interface Props {
+    data: object;
+}
+
+const PlayListCard = ({ data }: Props) => {
+    return (
+        <div
+            className={cx("my-play-list-card")}
+            style={{
+                backgroundImage: `url(${data.image})`
+            }}
+        >
+            <div className="play-list-image-shadow" />
+            <div className="play-list-content">
+                <p className="play-list-title">{data.title}</p>
+                <div className="play-list-counts">총 {data.counts || 0}곡</div>
+            </div>
+        </div>
+    );
+};
+
+export default PlayListCard;
