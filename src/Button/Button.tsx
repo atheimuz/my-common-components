6b5a599626446e47b7a73da59cacfa308c60400c
atheimuz <atheimuz@naver.com>
2024-04-25 18:27:29 +0900

    props 받아서 사용

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import cx from "classnames";
-import "./Button.scss";
+import React from "react"
+import cx from "classnames"
+import "./Button.scss"
 
 export interface Props {
-    children?: any;
-    className?: string;
-    theme?: string;
-    size?: string;
-    disabled?: boolean;
+    children?: any
+    className?: string
+    theme?: string
+    size?: string
+    disabled?: boolean
 }
 
 const Button = ({
@@ -22,12 +22,12 @@ const Button = ({
         <button
             type="button"
             className={cx("my-button", className, theme, size)}
-            disabled
+            disabled={disabled}
             {...rest}
         >
             <div>{children}</div>
         </button>
-    );
-};
+    )
+}
 
-export default Button;
+export default Button
