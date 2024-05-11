724a0e46f1b15fd3a7c294691a6ee8542f65b805
atheimuz <atheimuz@naver.com>
2024-05-11 19:27:21 +0900

    type 추가

--- src/Tooltip/Tooltip.tsx
+++ src/Tooltip/Tooltip.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import cx from "classnames"
 import "./Tooltip.scss"
 
@@ -6,6 +6,7 @@ export interface Props {
     children?: React.ReactNode
     className?: string
     title?: React.ReactNode
+    type: "keep" | "hover" | "click"
     direction: "top" | "bottom" | "left" | "right"
 }
 
@@ -13,13 +14,30 @@ const Tooltip = ({
     children,
     className = "",
     title,
+    type = "keep",
     direction = "bottom",
     ...rest
 }: Props) => {
+    const [status, setStatus] = useState(true)
+
     return (
-        <div className={cx("my-tooltip", className)} {...rest}>
+        <div className={cx("my-tooltip", className, type)} {...rest}>
             {children}
-            <div className={cx("my-tooltip-msg", direction)}>{title}</div>
+
+            {status && (
+                <div className={cx("my-tooltip-msg", direction)}>
+                    {title}
+                    {type === "click" && (
+                        <button
+                            type="button"
+                            className="my-tooltip-close-btn"
+                            onClick={() => setStatus(false)}
+                        >
+                            X
+                        </button>
+                    )}
+                </div>
+            )}
         </div>
     )
 }
