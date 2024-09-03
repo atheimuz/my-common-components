1b69236a5d9b00c6cdb3c95b3aaee81c3c19c522
atheimuz <atheimuz@naver.com>
2024-09-03 17:24:28 +0900

    Alert 하위 컴포넌트 분리

--- src/Alert/Alert.stories.tsx
+++ src/Alert/Alert.stories.tsx
@@ -1,23 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 import { Story, Meta } from "@storybook/react"
 
-import Alert, {Props} from "./Alert"
+import Alert, { Props } from "./Alert"
+import Button from "../Button"
 
 export default {
     title: "Example/Alert",
     component: Alert
 } as Meta
 
-const Template: Story<Props> = (args) => <Alert {...args} />
+const Template: Story<Props> = (args) => {
+    return (
+        <Alert {...args}>
+            <Alert.Header>타이틀 영역<Alert.CloseBtn /></Alert.Header>
+            <Alert.Content>본문 영역</Alert.Content>
+            <Alert.Footer>
+                <Button theme="tertiary">취소</Button>
+                <Button theme="primary">확인</Button>
+            </Alert.Footer>
+        </Alert>
+    )
+}
 
 export const Default = Template.bind({})
 Default.args = {
     children: "다시 시도해주세요."
-}
-Default.argTypes = {
-    status: {
-        control: {
-            type: "boolean"
-        }
-    }
 }
--- src/Alert/Alert.tsx
+++ src/Alert/Alert.tsx
@@ -1,33 +1,80 @@
-import React, { useState, useEffect } from "react"
+import React, { createContext, useContext } from "react"
 import cx from "classnames"
+import Overlay from "../Overlay"
 import "./Alert.scss"
 
 export interface Props {
-    children: any
-    className: string
-    status: boolean
+    children: React.ReactNode
+    className?: string
+    isOpen: boolean
+    onClose?: () => void
 }
 
-const Alert = ({ className, children, status, ...rest }: Props) => {
-    const [isActive, setIsActive] = useState(false)
+type AlertContextType = {
+    onClose?: () => void;
+};
 
-    useEffect(() => {
-        if (!status) {
-            setTimeout(() => {
-                setIsActive(status)
-            }, 1000)
-        } else {
-            setIsActive(status)
-        }
-    }, [status])
+const AlertContext = createContext<AlertContextType>({})
+const Alert = ({ className, children, isOpen, onClose, ...rest }: Props) => {
+    if (!isOpen) return null;
+
     return (
-        <div
-            className={cx("my-alert", className, { active: isActive })}
-            {...rest}
-        >
+        <AlertContext.Provider value={{ onClose }}>
+            <Overlay>
+                <div
+                    className={cx("my-alert", className)}
+                    {...rest}
+                >
+                    {children}
+                </div>
+            </Overlay>
+        </AlertContext.Provider>
+    )
+}
+
+const Header = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="my-alert-header">
             {children}
         </div>
     )
 }
+
+const Content = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="my-alert-content">
+            {children}
+        </div>
+    )
+}
+
+const Footer = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="my-alert-footer">
+            {children}
+        </div>
+    )
+}
+
+
+const CloseBtn = () => {
+    const { onClose } = useContext(AlertContext);
+
+    return (
+        <button
+            type="button"
+            className="my-alert-close-btn"
+            aria-label="닫기"
+            onClick={onClose}
+        >
+            X
+        </button>
+    )
+}
+
+Alert.Header = Header;
+Alert.Content = Content;
+Alert.Footer = Footer;
+Alert.CloseBtn = CloseBtn;
 
 export default Alert
