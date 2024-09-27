847ecd17fdb7167e6c89b291d32c59eff12d04a9
atheimuz <atheimuz@naver.com>
2024-09-28 00:18:15 +0900

    탭 컴포넌트 추가

---
+++ packages/react/src/components/Tab/Tab.stories.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react"
+import { Story, Meta } from "@storybook/react"
+
+import Tab, { ItemProps } from "./Tab"
+
+export default {
+    title: "Example/Tab",
+    component: Tab
+} as Meta
+
+const Template: Story<ItemProps> = () => {
+    const [value, setValue] = useState<number>(1)
+
+    return (
+        <Tab value={value} onChange={(val) => setValue(val as number)}>
+            <Tab.Item value={1}>목록1</Tab.Item>
+            <Tab.Item value={2}>목록2</Tab.Item>
+            <Tab.Item value={3}>목록3</Tab.Item>
+            <Tab.Item value={4}>목록4</Tab.Item>
+            <Tab.Item value={5}>목록5</Tab.Item>
+            <Tab.Item value={6}>목록6</Tab.Item>
+            <Tab.Item value={7}>목록7</Tab.Item>
+            <Tab.Item value={8}>목록8</Tab.Item>
+        </Tab>
+    )
+}
+
+export const Default = Template.bind({})
+Default.argTypes = {}
---
+++ packages/react/src/components/Tab/Tab.tsx
@@ -0,0 +1,76 @@
+import React, { useRef, useEffect } from "react"
+import cx from "classnames"
+import { TabProvider, useTabContext } from "../../context/TabContext"
+import "./Tab.scss"
+
+export interface Props {
+    className?: string
+    value: string | number
+    onChange: (value: string | number) => void
+    children: React.ReactElement[]
+}
+
+export interface ItemProps {
+    className?: string
+    value: string | number
+    children: React.ReactNode
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+const Tab = ({ className, value, onChange, children }: Props) => {
+    return (
+        <TabProvider value={value} onChange={onChange}>
+            <div className={cx("my-tab", className)} role="tablist">
+                <div className="my-tab-items">{children}</div>
+            </div>
+        </TabProvider>
+    )
+}
+
+export const TabItem = ({ className, value, children, onClick }: ItemProps) => {
+    const tabRef = useRef<(HTMLButtonElement | null)[]>([])
+    const { selectedValue, selectValue } = useTabContext()
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        selectValue(value)
+        onClick?.(e)
+    }
+
+    useEffect(() => {
+        if (selectedValue === value) {
+            const activeTab = tabRef.current
+            const tabContainer = activeTab.parentElement?.parentElement
+
+            if (tabContainer) {
+                const containerWidth = tabContainer.clientWidth
+                const tabOffsetLeft = activeTab.offsetLeft
+                const tabWidth = activeTab.clientWidth
+
+                const scrollLeftPosition =
+                    tabOffsetLeft - containerWidth / 2 + tabWidth / 2
+
+                tabContainer.scrollTo({
+                    left: scrollLeftPosition,
+                    behavior: "smooth"
+                })
+            }
+        }
+    }, [selectValue, value])
+
+    return (
+        <button
+            ref={tabRef}
+            role="tab"
+            className={cx("my-tab-item", className, {
+                active: selectedValue === value
+            })}
+            onClick={handleClick}
+        >
+            {children}
+        </button>
+    )
+}
+
+Tab.Item = TabItem
+
+export default Tab
